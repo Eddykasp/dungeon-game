3c1fbe4189dc90b64351031e9c144930f358d27a
Max Kasperowski <maxkasperowski@gmail.com>
2022-11-13 19:10:32 +0100

    expand test level

--- dungeon-game.py
+++ dungeon-game.py
@@ -28,10 +28,11 @@ class App:
 
     self.map = Map(GAME_WIDTH, GAME_HEIGHT, TILE_WIDTH)
 
-    self.map.load_map([["F,WT,WL",      "F,PP_0>OR_0;A_0;DR_0;T_0,WT",    "F,PP_1>OR_0;DEL_0,WR,WT"], 
-                       ["F,WL",         "F,WB",                           "F,WR"], 
-                       ["F,A_0,WB,WL",  "F,A_1,WB,WT",                    "F,A_2,DR_0,DB_0"]],
-                       ["AND_0", "OR_0>A_2","NOT_0>T_0", "T_0>DB_0", "DEL_0>A_1"])
+    self.map.load_map([["F,WT,WL",      "F,PP_0>OR_0;A_0;DR_0;DL_0;T_0,WT",    "F,PP_1>OR_0;DEL_0,WR,WT", "F,WL,WT"], 
+                       ["F,WL",         "F,WB",                           "F,WR",                    "F,WL"], 
+                       ["F,A_0,WB,WL",  "F,A_1,WB,WT",                    "F,A_2,DR_0,DB_0",         "F,DL_0"],
+                       ["F,WL,WT",      "F,WT",                           "F,DT_0",                  "F"]],
+                       ["AND_0", "OR_0>A_2","NOT_0>T_0", "T_0>DB_0;DT_0", "DEL_0>A_1"])
 
 
     # create random map for testing
