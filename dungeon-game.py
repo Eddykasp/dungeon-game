1ded7613334c45a7410f664d405df1a6f2613bb2
Max Kasperowski <maxkasperowski@gmail.com>
2022-11-13 17:43:35 +0100

    reduce player speed

--- dungeon-game.py
+++ dungeon-game.py
@@ -12,7 +12,7 @@ class Player(Entity):
   def __init__(self):
     self.x = 0
     self.y = 0
-    self.speed = 1
+    self.speed = 0.5
     self.width = 2
     self.height = 4
     self.wallCollider = True
